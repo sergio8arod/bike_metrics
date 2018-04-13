a3bfc3ecc04e98a280f18b1581d2d8b07c21877f
sergio8arod <sergio8arod@gmail.com>
2018-04-13 00:42:59 -0500

    email

--- application/config/email.php
+++ application/config/email.php
@@ -5,14 +5,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * email config file
  */
 
+$config['protocol'] = 'smtp';
 $config['useragent'] = 'CodeIgniter';
-$config['smtp_host'] = 'smtp.gmail.com';
-$config['smtp_port'] = '465';
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_port']    = '465';
+$config['smtp_timeout'] = '7';
 $config['smtp_user'] = 'sibucsys@mejorenbici.com';
 $config['smtp_pass'] = 'SIBUCmEb.4034';
 $config['smtp_crypto'] = 'ssl';
 $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
-$config['protocol'] = 'smtp';
-$config['wordwrap'] = TRUE;
+$config['newline']    = "\r\n";
+$config['validation'] = TRUE;
 
