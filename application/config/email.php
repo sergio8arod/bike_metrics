e91baf4913a3175cae49e3c24a176712df1fc39b
sergio8arod <sergio8arod@gmail.com>
2018-04-20 16:24:04 -0500

    mail

--- application/config/email.php
+++ application/config/email.php
@@ -11,10 +11,9 @@ $config['smtp_host'] = 'smtp.gmail.com';
 $config['smtp_port']    = '465';
 $config['smtp_timeout'] = '7';
 $config['smtp_user'] = 'sibucsys@mejorenbici.com';
-$config['smtp_pass'] = 'SIBUCmEb.4034';
+$config['smtp_pass'] = 'ww-W4h4@csTW43!b*K_fNmkTpD^$VqQS*Vr952RHc=6n';
 $config['smtp_crypto'] = 'ssl';
 $config['mailtype'] = 'text';
 $config['charset'] = 'utf-8';
 $config['newline']    = "\r\n";
 $config['validation'] = TRUE;
-
